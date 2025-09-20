9da615d1bf4be20e40ef8e31b84a695dd43663fa
yoprobotics <yoprobotics@gmail.com>
2025-09-20 10:08:07 -0400

    feat(ingest): add main index.ts export file

--- packages/ingest/index.ts
+++ packages/ingest/index.ts
@@ -1,7 +1,6 @@
-// Data ingestion and parsing utilities for AI Sales Agent
-export * from './csv-parser'
-export * from './url-scraper'
-export * from './normalizer'
-export * from './deduplicator'
-export * from './batch-processor'
-export * from './types'
+// Main export file for the ingest package
+export * from './csv-parser';
+export * from './url-scraper';
+export * from './normalizer';
+export * from './deduplicator';
+export * from './batch-processor';
